ded84e1cb49fed77764dfad7a6880812300417ff
cmoqrin <kato.teiji@classmethod.jp>
2021-10-19 19:36:51 +0900

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { ChakraProvider, Box, VStack, Flex, Spacer } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { ChakraProvider, Box, VStack } from '@chakra-ui/react';
 import Header from './components/Header';
 import customTheme from './extendTheme';
 
@@ -8,10 +7,6 @@ export const App = () => (
   <ChakraProvider theme={customTheme}>
     <Box fontSize='xl'>
       <Header />
-      <Flex w='100%'>
-        <Spacer />
-        <ColorModeSwitcher />
-      </Flex>
       <VStack spacing={8}></VStack>
     </Box>
   </ChakraProvider>
