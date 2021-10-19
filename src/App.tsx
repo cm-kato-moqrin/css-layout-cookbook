51061ccbcf97b7f5071c8f583b337a140dddac5e
cmoqrin <kato.teiji@classmethod.jp>
2021-10-19 18:26:58 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
-import { ChakraProvider, Box, VStack, Flex, Spacer } from '@chakra-ui/react';
+import { ChakraProvider, Box, Flex, Spacer } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import customTheme from './extendTheme';
 
 export const App = () => (
   <ChakraProvider theme={customTheme}>
-    <Box fontSize='xl'>
-      <Header />
-      <Flex w='100%'>
-        <Spacer />
-        <ColorModeSwitcher />
-      </Flex>
-      <VStack spacing={8}></VStack>
+    <Box fontSize='xl' mb='auto'>
+      <Box display='flex' flexDirection='column' minH='100vh'>
+        <Header />
+        <Flex w='100%'>
+          <Spacer />
+          <ColorModeSwitcher />
+        </Flex>
+        <Box p={4} mb='auto'>
+          Main page content here, add more if you want to see the footer push
+          down.
+        </Box>
+        <Footer />
+      </Box>
     </Box>
   </ChakraProvider>
 );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+function Footer() {
+  return (
+    <Box bg='#2D3748' p={4} color='white'>
+      Sticky footer
+    </Box>
+  );
+}
+
+export default Footer;
