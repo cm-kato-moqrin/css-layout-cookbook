bcbc4b9ab1d1b6ef9d306033d0c570d03f7dcabe
cmoqrin <kato.teiji@classmethod.jp>
2021-10-25 18:19:02 +0900

    add divider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChakraProvider, VStack } from '@chakra-ui/react';
+import { ChakraProvider, StackDivider, VStack } from '@chakra-ui/react';
 import CenterContainer from './components/CenterContainer';
 import ListWithBadge from './components/ListWithBadge';
 import GridWrapper from './components/GridWrapper';
@@ -7,7 +7,14 @@ import customTheme from './extendTheme';
 
 export const App = () => (
   <ChakraProvider theme={customTheme}>
-    <VStack border='2px' h='100px' borderColor='black' rounded='lg' spacing={5}>
+    <VStack
+      divider={<StackDivider borderColor='gray.300' />}
+      border='2px'
+      h='100px'
+      borderColor='black'
+      rounded='lg'
+      spacing={5}
+    >
       <CenterContainer />
       <ListWithBadge />
       <GridWrapper />
