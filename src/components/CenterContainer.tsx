2944ea95e3207aa0b5ec9374cb396c2fad4122f6
cmoqrin <kato.teiji@classmethod.jp>
2021-10-25 13:15:39 +0900

    fixed font and mt

--- src/components/CenterContainer.tsx
+++ src/components/CenterContainer.tsx
@@ -6,12 +6,13 @@ function CenterContainer() {
   return (
     <VStack border='2px' h='100px' borderColor='black' rounded='lg'>
       <Center
-        mt={4}
+        mt={3}
         border='2px'
         borderColor='black'
         w='10em'
         rounded='lg'
         p='20px'
+        textStyle='p'
       >
         I am centered!
       </Center>
