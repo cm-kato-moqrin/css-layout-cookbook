82ac7bc0024d466565be0daf3a86629b746ea1ef
cmoqrin <kato.teiji@classmethod.jp>
2021-10-25 17:48:45 +0900

    centered index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { ChakraProvider, Box, Grid } from '@chakra-ui/react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CardGrid from './components/CardGrid';
+import { CardGrid, Footer, Header } from './components';
 import customTheme from './extendTheme';
 
 export const App = () => (
   <ChakraProvider theme={customTheme}>
     <Box fontSize='xl' mb='auto'>
-    <Grid minH='100vh' templateRows='auto 1fr auto'>
+      <Grid minH='100vh' templateRows='auto 1fr auto'>
         <Header />
         <Box p={4} mb='auto'>
           <CardGrid />
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as BreadCrumbNav } from './BreadCrumbNav';
+export { default as CardGrid } from './CardGrid';
+export { default as CardItem } from './CardItem';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
