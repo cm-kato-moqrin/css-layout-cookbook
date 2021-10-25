6bef72b87d1e3835a37867a6e0f673028694a69b
cmoqrin <kato.teiji@classmethod.jp>
2021-10-25 13:38:37 +0900

    fixed font

--- src/components/CenterContainer.tsx
+++ src/components/CenterContainer.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 function CenterContainer() {
   return (
     <Center
-      mt={4}
+      mt={3}
       border='2px'
       borderColor='black'
       w='10em'
       rounded='lg'
       p='20px'
+      textStyle='p'
     >
       I am centered!
     </Center>
