2e383f30a28037c7bc1726b161066cf66e5d7f14
cmoqrin <kato.teiji@classmethod.jp>
2021-10-25 18:09:23 +0900

    wrap container

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
-import { ChakraProvider, Box, Grid } from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, Container } from '@chakra-ui/react';
 import { CardGrid, Footer, Header, Pagination } from './components';
 import customTheme from './extendTheme';
 
 export const App = () => (
   <ChakraProvider theme={customTheme}>
-    <Box fontSize='xl' mb='auto'>
-      <Grid minH='100vh' templateRows='auto 1fr auto'>
-        <Header />
-        <Box p={4} mb='auto'>
-          <CardGrid />
-          <Pagination />
-        </Box>
-        <Footer />
-      </Grid>
-    </Box>
+    <Container maxW='container.xl'>
+      <Box fontSize='xl' mb='auto'>
+        <Grid minH='100vh' templateRows='auto 1fr auto'>
+          <Header />
+          <Box p={4} mb='auto'>
+            <CardGrid />
+            <Pagination />
+          </Box>
+          <Footer />
+        </Grid>
+      </Box>
+    </Container>
   </ChakraProvider>
 );
