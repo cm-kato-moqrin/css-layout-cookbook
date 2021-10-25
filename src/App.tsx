3de358d9a8c563689e8d69271fe437913cfb2038
cmoqrin <kato.teiji@classmethod.jp>
2021-10-25 17:36:55 +0900

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import customTheme from './extendTheme';
@@ -7,14 +7,14 @@ import customTheme from './extendTheme';
 export const App = () => (
   <ChakraProvider theme={customTheme}>
     <Box fontSize='xl' mb='auto'>
-      <Box display='flex' flexDirection='column' minH='100vh'>
+      <Grid minH='100vh' templateRows='auto 1fr auto'>
         <Header />
-        <Box p={4} mb='auto'>
+        <Box p={4} textStyle='p'>
           Main page content here, add more if you want to see the footer push
           down.
         </Box>
         <Footer />
-      </Box>
+      </Grid>
     </Box>
   </ChakraProvider>
 );
